df3cee2710092e0a74dc22805dcb2f84e3e6220c
esakimedia <apachecordovax@gmail.com>
2022-07-05 19:21:38 +0200

    setting up the sign-out link

--- src/components/HeaderAuth.vue
+++ src/components/HeaderAuth.vue
@@ -1,5 +1,4 @@
 <template>
-    <!-- header -->
     <header class="py-4 shadow-sm bg-white">
         <div class="container flex items-center justify-between">
             <!-- logo -->
@@ -8,13 +7,25 @@
             </a>
 
             <!-- icons -->
-            <div class="flex items-center space-x-4">
-                <a href="#" class="text-center text-gray-700 hover:text-primary transition relative">
-                    <div class="text-2xl">
-                        <i class="far fa-user"></i>
+            <div class="flex items-center justify-between space-x-4">
+                <div class="text-xs leading-3">Login/Register</div>
+
+                <!-- Profile dropdown -->
+                <Menu as="div" class="ml-3 relative">
+                    <div>
+                    <MenuButton class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                        <span class="sr-only">Open user menu</span>
+                        <img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" />
+                    </MenuButton>
                     </div>
-                    <div class="text-xs leading-3">Login/Register</div>
-                </a>
+                    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                    <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <MenuItem>
+                            <a @click="logout" :class="['block px-4 py-2 text-sm text-gray-700 cursor-pointer']">Sign out</a>
+                        </MenuItem>
+                    </MenuItems>
+                    </transition>
+                </Menu>
             </div>
         </div>
     </header>
@@ -22,7 +33,24 @@
 </template>
 
 <script>
+    import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+    import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
+
     export default {
-        name: 'HeaderAuth'
+        name: 'HeaderAuth',
+        components: {
+            Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems,
+            BellIcon, MenuIcon, XIcon
+        },
+        data() {
+            return {
+                 user: {
+                    name: 'Tom Cook',
+                    email: 'tom@example.com',
+                    imageUrl:
+                        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                },
+            }
+        }
     }
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -46,7 +46,26 @@
                     <router-link to="/about" class="text-gray-200 hover:text-white transition">About us</router-link>
                     <router-link to="/contact" class="text-gray-200 hover:text-white transition">Contact us</router-link>
                 </div>
-                <router-link to="/auth" class="text-gray-200 hover:text-white transition">Login/Register</router-link>
+
+                <!-- Profile dropdown -->
+                <div class="flex items-center justify-between">
+                    <router-link to="/auth" class="text-gray-200 hover:text-white transition">Login/Register</router-link>
+                    <Menu as="div" class="ml-3 relative">
+                        <div>
+                        <MenuButton class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                            <span class="sr-only">Open user menu</span>
+                            <img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" />
+                        </MenuButton>
+                        </div>
+                        <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                        <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <MenuItem>
+                            <a @click="logout" :class="['block px-4 py-2 text-sm text-gray-700 cursor-pointer']">Sign out</a>
+                            </MenuItem>
+                        </MenuItems>
+                        </transition>
+                    </Menu>
+                </div>
             </div>
         </div>
     </nav>
@@ -54,7 +73,24 @@
 </template>
 
 <script>
+    import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+    import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
+
     export default {
-        name: 'Navbar'
+        name: 'Navbar',
+        components: {
+            Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems,
+            BellIcon, MenuIcon, XIcon
+        },
+        data() {
+            return {
+                 user: {
+                    name: 'Tom Cook',
+                    email: 'tom@example.com',
+                    imageUrl:
+                        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                },
+            }
+        }
     }
 </script>
