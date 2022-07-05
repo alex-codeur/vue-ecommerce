4a8a50a81733b1d4bc35a649a4d0edab0383ae15
esakimedia <apachecordovax@gmail.com>
2022-07-05 13:16:07 +0200

    setting up the register page

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Wishlist from '../views/public/Wishlist.vue'
 import Account from '../views/public/Account.vue'
 import Cart from '../views/public/Cart.vue'
 import Checkout from '../views/public/Checkout.vue'
+import Register from '../views/public/Register.vue'
 
 import NotFound from '../views/public/NotFound.vue'
 
@@ -24,7 +25,8 @@ const routes = [
             { path: '/wishlist', name: 'wishlist', component: Wishlist },
             { path: '/account', name: 'account', component: Account },
             { path: '/cart', name: 'cart', component: Cart },
-            { path: '/checkout', name: 'checkout', component: Checkout }
+            { path: '/checkout', name: 'checkout', component: Checkout },
+            { path: '/register', name: 'register', component: Register }
         ]
     },
     {
